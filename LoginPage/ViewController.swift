0fb4e1efe6ffe7fe7004be3c20e47243c1aaa8a7
Tanveer Jan <tjan90@yahoo.com>
2019-08-24 11:54:30 +0200

    Initial Commit

---
+++ LoginPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Tanveer Jan on 24/08/2019.
+//  Copyright © 2019 Tanveer Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
